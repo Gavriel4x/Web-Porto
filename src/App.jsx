1875e0ad7802abe1950d4ddc86785c9965f560e6
Gavriel <gavrielrey101@yahoo.com>
2026-03-24 00:37:53 +0700

    Homepage: Responsive, bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import work_experience from "./assets/data/work_experience.json";
 
 export default function App() {
 
@@ -8,8 +9,6 @@ export default function App() {
     const [displayDivFive, setDisplayDivFive] = useState("none");  
 
     useEffect(() => {
-        console.log("start display");
-
         const t1 = setTimeout(() => setDisplayDivTwo("revert"), 3000);
         const t2 = setTimeout(() => setDisplayDivThree("revert"), 6000);
         const t3 = setTimeout(() => setDisplayDivFour("revert"), 8750);
@@ -27,24 +26,27 @@ export default function App() {
         <div className="content-pane">
 
             <div className="bio">
-                <h2 style={{ fontSize: "20px", marginBottom: "36px" }}>&gt; hello_iam</h2>
+                <h2 className="bio-label">&gt; hello_iam</h2>
 
-                <h1 style={{color:"white", fontSize: "64px" , marginBottom: "9px"}}>Gavriel Reynard</h1>
+                <h1 className="bio-name">Gavriel Reynard</h1>
 
-                <h2 style={{ fontSize: "32px", marginBottom: "36px" }}>Front-End Developer</h2>
+                <h2 className="bio-title">Front-End Developer</h2>
 
+                <h2 className="text-bio" style={{ color: "#868686" }}>
 
-                <h2 className="text-bio" style={{ fontSize: "22px", color: "#868686" }}>
-                    
-                    <div>I’m <a className="text-bio-bold">passionate about building clean and scalable web</a></div>
-                    <div style={{display: displayDivTwo}}><a>applications</a>. I bring both technical expertise and</div>
+                    <div>I’m <a className="text-bio-bold">passionate in building clean scalable web</a></div>
+                    <div style={{display: displayDivTwo}}><a>applications</a>. I bring both technical expertise &</div>
                     <div style={{display: displayDivThree}}>strong product thinking skills to the products I</div>
                     <div style={{display: displayDivFour}}>build.</div>
                     <br/><br/>
                     <div style={{display: displayDivFive}}> Open <a style={{color: "#FEA2CD"}}>for</a> collaboration and opportunities.</div>
                 </h2>
             </div>
-
+            <div className="experience">
+                <div className="title-experience" style={{ display: "flex", alignItems:"center"}}>
+                    <h2 className="experience-label">EXPERIENCE</h2>
+                </div>
+            </div>
         </div>
     );
 }
