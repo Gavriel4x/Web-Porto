24001a7701e139d8ef3025d3d9bbcd214225528f
Gavriel <gavrielrey101@yahoo.com>
2026-03-27 15:01:38 +0700

    Contacts: fixes to responsive image content, fix responsive display on height

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -5,12 +5,15 @@ const githubLogo = "https://res.cloudinary.com/dykphdvdb/image/upload/v177453935
 const linkedinLogo = "https://res.cloudinary.com/dykphdvdb/image/upload/v1774539355/mingcute_linkedin-line_jjkt4p.svg";
 const emailLogo = "https://res.cloudinary.com/dykphdvdb/image/upload/v1774539354/ic_outline-email_telm3o.svg";
 const line = "https://res.cloudinary.com/dykphdvdb/image/upload/v1774539354/line-contacts_Desktop_laziqh.svg";
+const line_ipad = "https://res.cloudinary.com/dykphdvdb/image/upload/v1774595235/line_Ipad_jok7jj.svg";
+const line_iphone = "https://res.cloudinary.com/dykphdvdb/image/upload/v1774595235/line_Iphone_tnwbas.svg";
 
 const onHoverGithubLogo = "https://res.cloudinary.com/dykphdvdb/image/upload/v1774542724/Property_1_Variant2_tjg2eb.svg";
 const onHoverLinkedinLogo = "https://res.cloudinary.com/dykphdvdb/image/upload/v1774542724/Property_1_Variant2-1_ixwjsb.svg";
 const onHoverEmailLogo = "https://res.cloudinary.com/dykphdvdb/image/upload/v1774542735/Property_1_Variant2-2_vpflf0.svg";
 
 // 31x31, 2x703, 2x960 , 2x100
+const screenWidth = window.innerWidth;
 
 export default function contacts () {
 
@@ -33,7 +36,19 @@ export default function contacts () {
                     style={{cursor:"pointer"}}
                 alt="email logo" className="logo" /> 
             </div>
-            <img src={line} alt="line" className="line" /> 
+            <img src={(() => {
+                
+                if(screenWidth > 1024) {
+                    return line;
+                }
+                else if(screenWidth > 402) {
+                    return line_ipad
+                }
+                else {
+                    return line_iphone;
+                }
+
+            })()} alt="line" className="line" /> 
         </div>
     );
     
