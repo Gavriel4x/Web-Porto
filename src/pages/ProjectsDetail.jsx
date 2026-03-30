d28ee767716c7609bd80998b63e556184672d2f9
Gavriel <gavrielrey101@yahoo.com>
2026-03-30 21:48:04 +0700

    Fix: navbar width affected

--- src/pages/ProjectsDetail.jsx
+++ src/pages/ProjectsDetail.jsx
@@ -13,11 +13,11 @@ export default function ProjectsDetail() {
                 <div className="text-description text-jetbrains-mono-bold primary-green" style={{fontSize:"36px"}}>{projectData.description}</div>
                 <div className="container-top-inner" style={{display:"flex", flexDirection:"column", marginTop:"32px", gap:"26px"}}>
                     <div className="container-role" style={{display:"flex", flexDirection:"row", gap:"86px"}}>
-                        <div className="label label-role text-jetbrains-mono-regular white-70" style={{fontSize:"22px"}}>ROLE</div>
+                        <div className="label-projects-detail label-role text-jetbrains-mono-regular white-70" style={{fontSize:"22px"}}>ROLE</div>
                         <div className="text-role text-satoshi-regular white-70"  style={{fontSize:"22px"}}>{projectData.role}</div>
                     </div>
                     <div className="container-techstack" style={{display:"flex", flexDirection:"row", gap:"86px"}}>
-                        <div className="label label-techstack text-jetbrains-mono-regular white-70" style={{fontSize:"22px"}}>TECH</div>
+                        <div className="label-projects-detail label-techstack text-jetbrains-mono-regular white-70" style={{fontSize:"22px"}}>TECH</div>
                         <div className="wrapper-techstack" style={{display:"flex", flexDirection:"row", gap:"12px"}}>
                             {projectData.techstack.map((tech, techIndex) => (
                                 <div key={techIndex} className="techstack-project primary-green text-satoshi-regular"
@@ -31,7 +31,7 @@ export default function ProjectsDetail() {
                         </div>
                     </div>
                     <div className="container-url" style={{display:"flex", flexDirection:"row", gap:"86px"}}>
-                        <div className="label label-url text-jetbrains-mono-regular white-70" style={{fontSize:"22px"}}>URL</div>
+                        <div className="label-projects-detail label-url text-jetbrains-mono-regular white-70" style={{fontSize:"22px"}}>URL</div>
                         <a className="text-url text-satoshi-regular white-70"  style={{fontSize:"22px", cursor:"pointer", textDecoration:"none"}}
                             target="_blank"
                             href={projectData.github}
